3aa464ea6b83a11700a852de755fd13d1128adff
Mohammad Nurdin <nurdinnorazanservices@gmail.com>
2015-06-25 05:15:24 +0800

    Init commit.

---
+++ SwiftUiTableViewPickerScreenTests/SwiftUiTableViewPickerScreenTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUiTableViewPickerScreenTests.swift
+//  SwiftUiTableViewPickerScreenTests
+//
+//  Created by Mohammad Nurdin bin Norazan on 6/25/15.
+//  Copyright (c) 2015 Nurdin Norazan Services. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftUiTableViewPickerScreenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
